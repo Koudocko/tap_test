0c5b1204445904f9dd3d436619bb8b4d296c6eba
Koudocko <tyler@pcquest.ca>
2024-01-29 09:17:41 -0500

    Simplified logic I guess.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
   while (true){
     std::cout << "<=========================>" << std::endl;
 
-    int letter = std::rand() % 26, u_row{}, u_col{}, a_row{}, a_col{};
+    int letter = std::rand() % 26, u_row{}, u_col{};
     std::cout << "Letter: " << "\e[0;34m" << char(letter + 97) << "\e[0m " << std::endl;
 
     std::cin >> u_row >> u_col; 
@@ -17,12 +17,11 @@ int main(){
     }
 
     if (letter + 97 == 'k')
-      a_row = 1, a_col = 3;
-    else if (letter + 97 < 'k')
-      a_row = (letter / 5) + 1, a_col = (letter % 5) + 1;
-    else
-      a_row = ((letter - 1) / 5) + 1, a_col = ((letter - 1) % 5) + 1;
+      letter -= 10;
+    else if (letter + 97 > 'k')
+      --letter;
 
+    int a_row = (letter / 5) + 1, a_col = (letter % 5) + 1;
     if (u_row == a_row && u_col == a_col){
       std::cout << "\e[0;32mCorrect!\e[0m Letter " << char(letter + 97)
         << " is " << a_row << ":" << a_col << std::endl;
