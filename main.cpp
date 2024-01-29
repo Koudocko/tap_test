778500676e7b1e4c9acb3552669d433e49a4d27c
Koudocko <tyler@pcquest.ca>
2024-01-29 09:36:45 -0500

    Oopsie, bug fix with letter display and k adjustment.

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main(){
   while (true){
     std::cout << "<=========================>" << std::endl;
 
-    int letter = std::rand() % 26, u_row{}, u_col{};
-    std::cout << "Letter: " << "\e[0;34m" << char(letter + 97) << "\e[0m " << std::endl;
+    int offset = std::rand() % 26, u_row{}, u_col{};
+    char letter = offset + 97;
+    std::cout << "Letter: " << "\e[0;34m" << letter << "\e[0m " << std::endl;
 
     std::cin >> u_row >> u_col; 
     if (std::cin.fail()){
@@ -16,18 +17,18 @@ int main(){
       std::cin.ignore(std::numeric_limits<int>::max(), '\n');
     }
 
-    if (letter + 97 == 'k')
-      letter -= 10;
-    else if (letter + 97 > 'k')
-      --letter;
+    if (letter == 'k')
+      offset -= 8;
+    else if (letter > 'k')
+      --offset;
 
-    int a_row = (letter / 5) + 1, a_col = (letter % 5) + 1;
+    int a_row = (offset / 5) + 1, a_col = (offset % 5) + 1;
     if (u_row == a_row && u_col == a_col){
-      std::cout << "\e[0;32mCorrect!\e[0m Letter " << char(letter + 97)
+      std::cout << "\e[0;32mCorrect!\e[0m Letter " << letter
         << " is " << a_row << ":" << a_col << std::endl;
     }
     else{
-      std::cout << "\e[0;31mIncorrect!\e[0m Letter " << char(letter + 97)
+      std::cout << "\e[0;31mIncorrect!\e[0m Letter " << letter
         << " is " << a_row << ":" << a_col << std::endl;
     }
   }
